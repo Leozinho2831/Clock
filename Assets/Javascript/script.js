0dde6dfcebcb2e68003bdd2df78ed610fe730ecb
Leozinho2831 <leoferraz1818@gmail.com>
2023-10-10 13:54:25 -0300

    commit: new clock date and its settings

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,24 +1,41 @@
-const dadoHours = document.getElementById('hours');
-const dadoMinutes = document.getElementById('minutes');
-const dadoSeconds = document.getElementById('seconds');
+const cardHours = document.getElementById('hours');
+const cardMinutes = document.getElementById('minutes');
+const cardSeconds = document.getElementById('seconds');
+
+const cardSemana = document.getElementById('semana');
+
+const cardSegundos = document.getElementById('dia');
+const cardMinutos = document.getElementById('mes');
+const cardHoras = document.getElementById('ano');
 
 setInterval(function(){
-    let date = new Date();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-    let second = date.getSeconds();
+    let currentDate = new Date();
+    let hour = currentDate.getHours();
+    let minute = currentDate.getMinutes();
+    let second = currentDate.getSeconds();
 
-    if(hour < 10){
-        hour = '0' + hour;
-    }
-    if(minute < 10){
-        minute = '0' + minute;
-    }
-    if(second < 10){
-        second = '0' + second;
-    }
+    if(hour < 10) hour = '0' + hour;
+    if(minute < 10) minute = '0' + minute;
+    if(second < 10) second = '0' + second;
 
-    dadoHours.textContent = hour;
-    dadoMinutes.textContent = minute;
-    dadoSeconds.textContent = second;
+    cardHours.textContent = hour;
+    cardMinutes.textContent = minute;
+    cardSeconds.textContent = second;
+
+    const daysWeek = ['Domingo', 'Segunda Feira', 'Terça Feira', 'Quarta Feira', 'Quinta Feira', 'Sexta Feira', 'Sábado'];
+
+    let week = currentDate.getDay();
+    cardSemana.textContent = daysWeek[week];
+
+    let years = currentDate.getFullYear();
+    let months = currentDate.getMonth() + 1;
+    let days = currentDate.getDate();
+
+    if(days < 10) days = '0' + days;
+    if(months < 10) months = '0' + months;
+    if(years < 10) years = '0' + years;
+
+    cardSegundos.textContent = days;
+    cardMinutos.textContent = months;
+    cardHoras.textContent = years;
 });
